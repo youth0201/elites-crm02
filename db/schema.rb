f4eba40dabb11d3d6337c889286b8798339ec7db
Yusuke Shimizu <youth0201@gmail.com>
2016-11-02 10:13:43 +0000

    06_実習 完了

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102085322) do
+ActiveRecord::Schema.define(version: 20161102093557) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       null: false
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20161102085322) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "family_name"
+    t.string   "given_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
