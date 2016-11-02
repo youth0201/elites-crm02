95e35c6a92498ea3a458af1e107e956a2e24a7ae
Yusuke Shimizu <youth0201@gmail.com>
2016-11-02 10:47:58 +0000

    07_実習 完了

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,46 @@
+class CommentsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to customer_path(@comment.customer_id)
+    else
+      redirect_to customer_path(@comment.customer_id)
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    # @commentがdestroyされる前に、commentが誰のものかを変数に保存する
+    customer_id = @comment.customer_id
+    @comment.destroy
+    # さっき保存したcustomer_idをここで使う
+    redirect_to customer_path(customer_id)
+  end
+  
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :customer_id)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :customers
   root 'customers#index'
 
+  resources :comments, only: [:create, :destroy]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
